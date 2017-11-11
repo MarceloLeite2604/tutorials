3bfb5f52973fb4850ad7ba391b2a5bc97b6b3cb9
MarceloLeite2604 <marceloleite2604@gmail.com>
2017-11-11 16:09:05 -0200

    Advanced on Spring MVC 2 course.

--- alura/spring-mvc-2/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ alura/spring-mvc-2/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,17 +1,31 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
 
 @Controller
 @Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class HomeController {
 
+	@Autowired
+	private ProdutoDAO produtoDAO;
+
 	@RequestMapping("/")
-	public String index() {
+	public ModelAndView index() {
 		System.out.println("Entrando na home da CDC.");
-		return "home";
+		
+		List<Produto> produtos = produtoDAO.listar();
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
 	}
 }
