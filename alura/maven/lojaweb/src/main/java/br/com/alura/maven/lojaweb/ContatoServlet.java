1fa01d32c4585fdf669ce9a0db60743df6ab3292
MarceloLeite2604 <marceloleite2604@gmail.com>
2017-11-28 08:07:25 -0200

    Advanced on Alura's Maven course.

--- alura/maven/lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
+++ alura/maven/lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
@@ -9,13 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.maven.Produto;
+import br.com.caelum.stella.tinytype.CPF;
+
 @WebServlet(urlPatterns={"/contato"})
 public class ContatoServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Produto produto = new Produto("bala", 0.15); 
+		new CPF("01120711045").isValido();
+		
 		PrintWriter writer = resp.getWriter();
-		writer.println("<html><h2>Entre em Contato.</h2></html>");
+		writer.println("<html><h2>Entre em contato conosco.</h2></html>");
 		writer.close();
 	}
 	
