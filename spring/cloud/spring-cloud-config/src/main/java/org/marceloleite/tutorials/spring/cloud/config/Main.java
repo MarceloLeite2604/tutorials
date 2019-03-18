71d3a985b6e4e22d031db198a3c0d7f078a2443c
MarceloLeite2604 <marceloleite2604@gmail.com>
2019-03-18 09:31:25 -0300

    Cria um projeto para testar a disponibilização de informações do projeto git através do Spring.

---
+++ spring/cloud/spring-cloud-config/src/main/java/org/marceloleite/tutorials/spring/cloud/config/Main.java
@@ -0,0 +1,28 @@
+package org.marceloleite.tutorials.spring.cloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+public class Main {
+
+  @Value("${config.name}")
+  String name = "World";
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello " + name;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+
+}
+
---
+++ spring/cloud/spring-git-information/src/main/java/org/marceloleite/tutorials/spring/git/information/Main.java
@@ -0,0 +1,12 @@
+package org.marceloleite.tutorials.spring.git.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+  
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
