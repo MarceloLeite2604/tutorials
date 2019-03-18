099e23d7715b43add554644635357b720c8770b6
MarceloLeite2604 <marceloleite2604@gmail.com>
2019-03-18 15:14:39 -0300

    Revisita aos projetos Eureka client, feign client e embedded server.

---
+++ spring/cloud/spring-eureka-client/src/main/java/org/marceloleite/tutorials/spring/eureka/client/Main.java
@@ -0,0 +1,14 @@
+package org.marceloleite.tutorials.spring.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	
+}
---
+++ spring/cloud/spring-eureka-feign-client/src/main/java/org/marceloleite/tutorials/spring/eureka/feign/client/GreetingFeignClient.java
@@ -0,0 +1,11 @@
+package org.marceloleite.tutorials.spring.eureka.feign.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("spring-cloud-eureka-client")
+public interface GreetingFeignClient {
+	
+	@RequestMapping("eureka-client/greeting")
+	String greeting();
+}
