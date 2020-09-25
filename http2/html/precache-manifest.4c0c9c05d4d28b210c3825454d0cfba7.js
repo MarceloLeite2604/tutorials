c027b1e4e27b730c6a7f06e7b7658b1fae4cc960
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-09-25 13:30:38 -0300

    Add HTTP2 example

---
+++ http2/html/precache-manifest.4c0c9c05d4d28b210c3825454d0cfba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392872fbd48ab73b03333346703c8f15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f0f92e7ffdc049b0bf2",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "d4fee58afb9cbcd4eeca",
+    "url": "/static/js/2.92181a1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.92181a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0f92e7ffdc049b0bf2",
+    "url": "/static/js/main.1cee0468.chunk.js"
+  },
+  {
+    "revision": "e94a08ea4ad063695307",
+    "url": "/static/js/runtime-main.4888170a.js"
+  }
+]);
